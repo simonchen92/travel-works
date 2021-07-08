2c38afc1deef0ab045f28527defe14766e30d202
simonchen92 <simon.k.chen92@gmail.com>
2021-07-08 13:36:41 -0400

    refactor(files): delete unused code

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,9 @@
-import * as React from "react"
-import PropTypes from "prop-types"
+import React from "react"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+const Header = () => {
+  return <h1>Header</h1>
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
