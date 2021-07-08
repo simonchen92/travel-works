426f7a6507f66878c6abb7746489263222b343b5
simonchen92 <simon.k.chen92@gmail.com>
2021-07-08 13:38:07 -0400

    chore(npm): install styled component npm packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Triptastic`,
+    description: `Travel website showcasing the best travel destinations and deals.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
