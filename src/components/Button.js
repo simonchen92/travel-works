e5486b2b99d41f39d1f83731a4b692cdc16900ee
simonchen92 <simon.k.chen92@gmail.com>
2021-09-14 13:24:45 -0400

    style(button): added a different color to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import { Link } from "gatsby"
 
 export const Button = styled(Link)`
-  background: ${({ primary }) => (primary ? "#F26A2E" : "#077BF1")};
+  background: ${({ primary }) => (primary ? "#F26A2E" : "#2EB6F2")};
   white-space: nowrap;
   padding: ${({ big }) => (big ? "16px 40px" : "10px 32px")};
   color: #fff;
@@ -16,7 +16,7 @@ export const Button = styled(Link)`
   border-radius: ${({ round }) => (round ? "50px" : "none")};
 
   &:hover {
-    background: ${({ primary }) => (primary ? "#077BF1" : "#F26A2E")};
+    background: ${({ primary }) => (primary ? "#2EB6F2" : "#F26A2E")};
     transform: translateY(-2px);
   }
 `
